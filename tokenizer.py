343a82c1b776dfd3525255f7ab6d325d9f259256
michaelpginn <milespergallon271@gmail.com>
2023-10-27 15:34:02 -0600

    Initial commit

---
+++ tokenizer.py
@@ -0,0 +1,63 @@
+from transformers import PreTrainedTokenizer
+from collections import Counter
+
+class WhitespaceTokenizer(PreTrainedTokenizer):
+    def __init__(self, vocab=None, max_length=1024, **kwargs):
+        super().__init__(**kwargs)
+        self.unk_token = "[UNK]"
+        self.cls_token = "[CLS]"
+        self.sep_token = "[SEP]"
+        self.model_max_length = max_length
+        self.vocab = vocab if vocab else {self.unk_token: 0, self.cls_token: 1, self.sep_token: 2}
+
+    @property
+    def vocab_size(self) -> int:
+        return len(self.vocab)
+
+    def _tokenize(self, text, **kwargs):
+        return text.split()
+
+    def _convert_token_to_id(self, token):
+        return self.vocab.get(token, self.vocab.get(self.unk_token))
+
+    def convert_ids_to_tokens(self, ids, **kwargs):
+        id_to_token = {id: token for token, id in self.vocab.items()}
+        return [id_to_token.get(id, self.unk_token) for id in ids]
+
+    def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
+        if token_ids_1 is None:
+            return [self.vocab[self.cls_token]] + token_ids_0 + [self.vocab[self.sep_token]]
+        else:
+            return [self.vocab[self.cls_token]] + token_ids_0 + [self.vocab[self.sep_token]] + token_ids_1 + [self.vocab[self.sep_token]]
+
+    def learn_vocab(self, sentences, min_freq=1):
+        """Learn a vocabulary from a list of sentences."""
+        token_freqs = Counter()
+        for sentence in sentences:
+            tokens = self._tokenize(sentence)
+            token_freqs.update(tokens)
+
+        # Remove tokens that don't meet the minimum frequency threshold
+        token_freqs = {k: v for k, v in token_freqs.items() if v >= min_freq}
+
+        # Assign IDs to each token
+        next_id = max(self.vocab.values()) + 1  # Start after the last special token ID
+        for token, freq in token_freqs.items():
+            if token not in self.vocab:
+                self.vocab[token] = next_id
+                next_id += 1
+
+    def tokenize_batch(self, batch):
+        """Tokenize a batch of text using the custom tokenizer"""
+        inputs = [self.tokenize(text) for text in batch["text"]]
+        input_ids = [self.convert_tokens_to_ids(tokens) for tokens in inputs]
+        input_ids = [self.build_inputs_with_special_tokens(ids) for ids in input_ids]
+
+        input_ids = [ids[:self.model_max_length] + [0] * (self.model_max_length - len(ids)) for ids in input_ids]
+        attention_mask = [[1] * min(len(ids), self.model_max_length) + [0] * (self.model_max_length - len(ids)) for ids in
+                          input_ids]
+
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        }
---
+++ train.py
@@ -0,0 +1,63 @@
+import click
+import torch
+import wandb
+import datasets
+from tokenizer import WhitespaceTokenizer
+from transformers import BertConfig, BertForSequenceClassification, TrainingArguments, Trainer
+import evaluate
+import random
+import numpy as np
+
+device = "cuda:0" if torch.cuda.is_available() else "mps"
+
+metric = evaluate.load("accuracy")
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+@click.command()
+def train(batch_size=16, train_epochs=100, seed=0):
+    random.seed(seed)
+    wandb.init(project='latent-trees-agreement', entity="michael-ginn", name='transformer-no-ft', config={
+        "random-seed": seed,
+        "epochs": train_epochs,
+    })
+
+    dataset = datasets.load_dataset("michaelginn/latent-trees-agreement")
+    tokenizer = WhitespaceTokenizer()
+    tokenizer.learn_vocab([row['text'] for row in dataset['train']])
+    dataset = dataset.map(tokenizer.tokenize_batch, batched=True)
+
+    # Create random initialized BERT model
+    config = BertConfig(vocab_size=tokenizer.vocab_size, num_labels=2, max_position_embeddings=tokenizer.model_max_length)
+    model = BertForSequenceClassification(config=config).to(device)
+
+    args = TrainingArguments(
+        output_dir=f"./training-checkpoints",
+        evaluation_strategy="epoch",
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        gradient_accumulation_steps=3,
+        save_strategy="epoch",
+        save_total_limit=3,
+        num_train_epochs=train_epochs,
+        load_best_model_at_end=False,
+        logging_strategy='epoch',
+        report_to='wandb'
+    )
+
+
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test'],
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+
+if __name__ == "__main__":
+    train()
