178bdee12bb9d1afc24ae36d0feb020dbf3ae9e1
michaelpginn <milespergallon271@gmail.com>
2023-12-13 01:30:38 -0700

    Fix tree attention

--- src/TreeTransformer.py
+++ src/TreeTransformer.py
@@ -21,7 +21,8 @@ from transformers.models.bert.modeling_bert import (
     BertAttention,
     BertIntermediate,
     BertOutput,
-    BertLayer
+    BertLayer,
+    BertSelfAttention
 )
 from transformers.modeling_outputs import (
     BaseModelOutputWithPoolingAndCrossAttentions,
@@ -43,38 +44,7 @@ from dataclasses import dataclass
 
 logger = logging.get_logger(__name__)
 
-class TreeBertSelfAttention(nn.Module):
-    def __init__(self, config, position_embedding_type=None):
-        super().__init__()
-        if config.hidden_size % config.num_attention_heads != 0 and not hasattr(config, "embedding_size"):
-            raise ValueError(
-                f"The hidden size ({config.hidden_size}) is not a multiple of the number of attention "
-                f"heads ({config.num_attention_heads})"
-            )
-
-        self.num_attention_heads = config.num_attention_heads
-        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
-
-        self.query = nn.Linear(config.hidden_size, self.all_head_size)
-        self.key = nn.Linear(config.hidden_size, self.all_head_size)
-        self.value = nn.Linear(config.hidden_size, self.all_head_size)
-
-        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
-        self.position_embedding_type = position_embedding_type or getattr(
-            config, "position_embedding_type", "absolute"
-        )
-        if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
-            self.max_position_embeddings = config.max_position_embeddings
-            self.distance_embedding = nn.Embedding(2 * config.max_position_embeddings - 1, self.attention_head_size)
-
-        self.is_decoder = config.is_decoder
-
-    def transpose_for_scores(self, x: torch.Tensor) -> torch.Tensor:
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-        x = x.view(new_x_shape)
-        return x.permute(0, 2, 1, 3)
-
+class TreeBertSelfAttention(BertSelfAttention):
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -84,6 +54,7 @@ class TreeBertSelfAttention(nn.Module):
         encoder_attention_mask: Optional[torch.FloatTensor] = None,
         past_key_value: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
         output_attentions: Optional[bool] = False,
+        constituent_attention_probs: Optional[torch.FloatTensor] = None
     ) -> Tuple[torch.Tensor]:
         mixed_query_layer = self.query(hidden_states)
 
@@ -153,6 +124,11 @@ class TreeBertSelfAttention(nn.Module):
             # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
             attention_scores = attention_scores + attention_mask
 
+        print('constituents', constituent_attention_probs)
+
+        # Component-wise multiplication between attention scores (batches * heads * sequence * sequence) and constituent attention (batches * sequence * sequence)
+        attention_scores = attention_scores * constituent_attention_probs.unsqueeze(1).repeat(1, self.num_attention_heads, 1, 1)
+
         # Normalize the attention scores to probabilities.
         attention_probs = nn.functional.softmax(attention_scores, dim=-1)
 
@@ -195,7 +171,7 @@ class TreeBertAttention(BertAttention):
         output_attentions: Optional[bool] = False,
         constituent_attention_probs: Optional[torch.FloatTensor] = None
     ) -> Tuple[torch.Tensor]:
-        self_outputs = self.self(
+        self_outputs = self.self.forward(
             hidden_states,
             attention_mask,
             head_mask,
@@ -203,6 +179,7 @@ class TreeBertAttention(BertAttention):
             encoder_attention_mask,
             past_key_value,
             output_attentions,
+            constituent_attention_probs=constituent_attention_probs
         )
         attention_output = self.output(self_outputs[0], hidden_states)
         outputs = (attention_output,) + self_outputs[1:]  # add attentions if we output them
@@ -358,19 +335,14 @@ class TreeBertLayerOutputs:
     neighboring_attention: Union[None | torch.Tensor]
 
 
-class TreeBertLayer(nn.Module):
+class TreeBertLayer(BertLayer):
     def __init__(self, config, disable_treeing=False):
-        super().__init__()
-        self.chunk_size_feed_forward = config.chunk_size_feed_forward
-        self.seq_len_dim = 1
+        super().__init__(config=config)
         self.attention = TreeBertAttention(config)
-
         self.group_attention = GroupAttention(config)
         if config.is_decoder:
             raise Exception("Cannot initialize TreeBertLayer in decoder mode, unsupported")
 
-        self.intermediate = BertIntermediate(config)
-        self.output = BertOutput(config)
         self.disable_treeing = disable_treeing
 
     def forward(
@@ -411,12 +383,6 @@ class TreeBertLayer(nn.Module):
                                     attention_outputs=self_attention_outputs[1:],
                                     constituent_attention_output=constituent_attention_output,
                                     neighboring_attention=neighboring_attention)
-
-    def feed_forward_chunk(self, attention_output):
-        intermediate_output = self.intermediate(attention_output)
-        layer_output = self.output(intermediate_output, attention_output)
-        return layer_output
-
 
 class BaseModelOutputWithPastAndCrossAttentionsAndConstituentAttention(BaseModelOutputWithPastAndCrossAttentions):
     break_probs: Optional[Tuple[torch.FloatTensor]] = None
@@ -514,8 +480,8 @@ class TreeBertEncoder(nn.Module):
                 if self.config.add_cross_attention:
                     all_cross_attentions = all_cross_attentions + (layer_outputs.attention_outputs[1],)
 
-            # previous_group_probs = layer_outputs[1]
-            # break_probs.append(layer_outputs[2])
+            previous_group_probs = layer_outputs.constituent_attention_output
+            break_probs.append(layer_outputs.neighboring_attention)
 
         if output_hidden_states:
             all_hidden_states = all_hidden_states + (hidden_states,)
