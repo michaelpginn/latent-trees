a0d5a49cc46653a3843353febca412d8ff95f6e3
michaelpginn <milespergallon271@gmail.com>
2023-12-24 16:40:43 -0600

    Use curc

--- scripts/blanca-pretrain.sh
+++ scripts/blanca-pretrain.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1           # Number of requested nodes
-#SBATCH --gres=gpu:v100
+#SBATCH --gres=gpu
 #SBATCH --ntasks=4          # Number of requested cores
 #SBATCH --mem=32G
-#SBATCH --time=12:00:00          # Max walltime              # Specify QOS
-#SBATCH --qos=blanca-kann
-#SBATCH --out=train.%j.out      # Output file name
-#SBATCH --error=train.%j.err
+#SBATCH --time=7-00:00:00          # Max walltime              # Specify QOS
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --out=pretrain.%j.out      # Output file name
+#SBATCH --error=pretrain.%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=michael.ginn@colorado.edu
 
